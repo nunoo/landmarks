148cb29dfc2a7ea2f353d39b4c5d589b8c15f10e
Shawn Nunoo <snunoo@bitwiseindustries.com>
2023-06-12 10:41:47 -0700

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by nunoo on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
