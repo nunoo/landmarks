646a6c04bccfb1eb731d5190d472666bd661675e
nunoo <shawnnunoo@gmail.com>
2023-06-14 10:39:43 -0700

    feat(badges): add custom badge

---
+++ landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,26 @@
+//
+//  RotatedBadgeSymbol.swift
+//  landmarks
+//
+//  Created by nunoo on 6/13/23.
+//
+
+import SwiftUI
+
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
